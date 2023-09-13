0afc995c773e9cedee83275856bc9672c468a9de
Solnijko <mosmosmaxix@gmail.com>
2023-09-13 15:55:00 +0300

    Now each color gets it's name from https://www.thecolorapi.com/API

--- script.js
+++ script.js
@@ -2,21 +2,30 @@ const change_btn = document.getElementById("change-btn");
 const main = document.querySelector(".main");
 const hex_code = document.querySelector(".hex-code");
 
-
-function genColor(){
+// Generate random color
+function genColor() {
     const chars = "ABCDEF1234567890";
-    let color = "#";
+    let color = "";
     let counter = 0;
-    while (counter < 6){
+    while (counter < 6) {
         color += chars.charAt(Math.floor(Math.random() * chars.length));
         counter++;
     };
     return color;
 };
 
-change_btn.onclick = function(){
+// Changing background color and color description after clicking the button
+change_btn.onclick = function () {
     let new_color = genColor()
-    main.style.background = new_color;
-    hex_code.innerHTML = new_color;
-    hex_code.style.color = new_color;
+
+    // Change color and add description after successful request
+    fetch("https://www.thecolorapi.com/id?hex=" + new_color)
+        .then((response) => response.json())
+        .then((responseJSON) => {
+            hex_code.innerHTML = ("#" + new_color + " " + responseJSON.name.value);
+            main.style.background = ("#" + new_color);
+            hex_code.style.color = ("#" + new_color);
+        });
+
+
 };
