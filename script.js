c04a3e2b89e7f7d518a7317c31067b9bdf39235b
solnijko <fannomas88@gmail.com>
2023-11-17 00:36:24 +0300

    implemented image search based on unsplash api

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const change_btn = document.getElementById("change-btn");
 const main = document.querySelector(".main");
 const hex_code = document.querySelector(".hex-code");
+const image = document.querySelector(".image-container");
 
 // Generate random color
 function genColor() {
@@ -14,18 +15,34 @@ function genColor() {
     return color;
 };
 
+function getRandomImageByHex(color) {
+    let data;
+    // fetch(`https://api.unsplash.com/photos/random?client_id=eABgtWoa5JmvYg6H5QkN9jcB8RQMf5fN9AJY7jj6se4&color:#${hexCode}`)
+    fetch(`https://api.unsplash.com/search/photos?query=color ${color}`, {
+        headers:{
+            Authorization: "Client-ID eABgtWoa5JmvYg6H5QkN9jcB8RQMf5fN9AJY7jj6se4",
+            // color: `#${hexCode}`
+        }
+    })
+    .then((response) => response.json())
+    .then((responseJSON) =>{
+        console.log(responseJSON)
+        image.style.background = "url(" + responseJSON.results[0].urls.small + ")";
+    });
+    return data;
+}
+
 // Changing background color and color description after clicking the button
 change_btn.onclick = function () {
-    let new_color = genColor()
-
+    let new_color = genColor();
     // Change color and add description after successful request
     fetch("https://www.thecolorapi.com/id?hex=" + new_color)
         .then((response) => response.json())
         .then((responseJSON) => {
+            getRandomImageByHex(responseJSON.name.value);
             hex_code.innerHTML = ("#" + new_color + " " + responseJSON.name.value);
             main.style.background = ("#" + new_color);
             hex_code.style.color = ("#" + new_color);
-        });
-
-
+            
+        });   
 };
