717aa03deabb18670a189303d6efc0c8e6f1aecd
Solnijko <mosmosmaxix@gmail.com>
2023-11-17 15:25:58 +0300

    Polished image search implementation

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ import auth from "./config.js";
 const change_btn = document.getElementById("change-btn");
 const main = document.querySelector(".main");
 const hex_code = document.querySelector(".hex-code");
-const image = document.querySelector(".image-container");
+const missing = document.querySelector(".missing");
+const image = document.querySelector(".image");
 
 // Generate random color
 function genColor() {
@@ -17,34 +18,41 @@ function genColor() {
     return color;
 };
 
-function getRandomImageByHex(color) {
+// Gets an image from unsplash api and displays it inside the image container
+function getImage(color) {
     let data;
-    // fetch(`https://api.unsplash.com/photos/random?client_id=eABgtWoa5JmvYg6H5QkN9jcB8RQMf5fN9AJY7jj6se4&color:#${hexCode}`)
-    fetch(`https://api.unsplash.com/search/photos?query=color ${color}`, {
+    fetch(`https://api.unsplash.com/search/photos?query=${color}`, {
         headers:{
             Authorization: auth,
-            // color: `#${hexCode}`
-        }
-    })
-    .then((response) => response.json())
-    .then((responseJSON) =>{
-        console.log(responseJSON)
-        image.style.background = "url(" + responseJSON.results[0].urls.small + ")";
-    });
+        }})
+        .then((response) => response.json())
+        .then((responseJSON) =>{
+            if (responseJSON.results.length != 0){
+                image.src = responseJSON.results[0].urls.small;
+                missing.classList.remove(".hidden")
+            }
+            else{
+                missing.classList.add(".hidden")
+                image.src = "";
+            };
+        });   
     return data;
 }
 
 // Changing background color and color description after clicking the button
 change_btn.onclick = function () {
     let new_color = genColor();
+
     // Change color and add description after successful request
     fetch("https://www.thecolorapi.com/id?hex=" + new_color)
         .then((response) => response.json())
         .then((responseJSON) => {
-            getRandomImageByHex(responseJSON.name.value);
+
+            // Get a random image based on image description
+            getImage(responseJSON.name.value);
             hex_code.innerHTML = ("#" + new_color + " " + responseJSON.name.value);
             main.style.background = ("#" + new_color);
             hex_code.style.color = ("#" + new_color);
-            
-        });   
+        });
 };
+
